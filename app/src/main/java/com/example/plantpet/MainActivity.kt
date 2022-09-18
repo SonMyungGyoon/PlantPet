772b790b90365b3488dba4fe284f516bb63afb3e
StreetCat <ddi1235@yu.ac.kr>
2022-09-18 20:44:51 +0900

    sensor layout

--- app/src/main/java/com/example/plantpet/MainActivity.kt
+++ app/src/main/java/com/example/plantpet/MainActivity.kt
@@ -7,7 +7,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.plantpet.databinding.FragmentSensorBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
@@ -16,12 +18,16 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
+    private var bindSensor : FragmentSensorBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         auth = Firebase.auth
         super.onCreate(savedInstanceState)
+        bindSensor = FragmentSensorBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main)
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        val myRef : DatabaseReference = database.getReference("sensor/arduinocode/")
 //        binding.logoutBtn.setOnClickListener {
 //            auth.signOut()
 //            val intent = Intent(this, IntroActivity::class.java)
@@ -29,6 +35,21 @@ class MainActivity : AppCompatActivity() {
 //            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 //            startActivity(intent)
 //        }
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val pot_temperature = dataSnapshot.child("pot_temperature").value
+                Toast.makeText(baseContext, "값 가져오기", Toast.LENGTH_LONG).show()
+                bindSensor!!.temptext.text = pot_temperature.toString() + "°C"
+            }
+            override fun onCancelled(error: DatabaseError) {  // Failed to read value
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })
+
+
+
+
 
 
 
--- app/src/main/java/com/example/plantpet/fragments/SensorFragment.kt
+++ app/src/main/java/com/example/plantpet/fragments/SensorFragment.kt
@@ -1,15 +1,20 @@
 package com.example.plantpet.fragments
 
+import android.content.ContentValues
 import android.database.DatabaseUtils
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.navigation.findNavController
 import com.example.plantpet.R
 import com.example.plantpet.databinding.FragmentSensorBinding
+import com.google.firebase.database.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +32,7 @@ class SensorFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -34,7 +40,23 @@ class SensorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        //val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        //val myRef : DatabaseReference = database.getReference("sensor/arduinocode")
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sensor, container, false)
+
+        val bind = FragmentSensorBinding.inflate(inflater, container, false)
+
+        /*myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val pot_temperature = dataSnapshot.child("pot_temperature").value
+                bind.temptext.text = pot_temperature.toString() + "°C"
+            }
+            override fun onCancelled(error: DatabaseError) {  // Failed to read value
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })*/
 
         binding.hometap.setOnClickListener{
             it.findNavController().navigate(R.id.action_sensorFragment_to_homeFragment)
@@ -50,6 +72,10 @@ class SensorFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    fun changeTempText(string: String){
+        binding.temptext.text = string
     }
 
 }
