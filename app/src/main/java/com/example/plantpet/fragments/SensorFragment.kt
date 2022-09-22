fb80ca804f2a452600100245e65cad1b80b4e763
StreetCat <ddi1235@yu.ac.kr>
2022-09-22 22:52:17 +0900

    get firebase sensor value

--- app/src/main/java/com/example/plantpet/fragments/SensorFragment.kt
+++ app/src/main/java/com/example/plantpet/fragments/SensorFragment.kt
@@ -1,16 +1,11 @@
 package com.example.plantpet.fragments
 
-import android.content.ContentValues
-import android.database.DatabaseUtils
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.navigation.findNavController
 import com.example.plantpet.R
 import com.example.plantpet.databinding.FragmentSensorBinding
@@ -32,32 +27,30 @@ class SensorFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
 
-        //val database : FirebaseDatabase = FirebaseDatabase.getInstance()
-        //val myRef : DatabaseReference = database.getReference("sensor/arduinocode")
-
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sensor, container, false)
-
-        val bind = FragmentSensorBinding.inflate(inflater, container, false)
-
-        /*myRef.addValueEventListener(object : ValueEventListener {
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        val myRef : DatabaseReference = database.getReference("sensor/arduinocode")
+        myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val pot_temperature = dataSnapshot.child("pot_temperature").value
-                bind.temptext.text = pot_temperature.toString() + "°C"
+                val pot_humidity = dataSnapshot.child("pot_humidity").value
+                val pot_light = dataSnapshot.child("light").value
+                val pot_soil = dataSnapshot.child("soil_humidity").value
+                binding.temptext.text = pot_temperature.toString() + "°C"
+                binding.humitext.text = pot_humidity.toString() + "%"
+                binding.lighttext.text = pot_light.toString() + "%"
+                binding.soiltext.text = pot_soil.toString() + "%"
             }
             override fun onCancelled(error: DatabaseError) {  // Failed to read value
-                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
             }
-        })*/
-
+        })
         binding.hometap.setOnClickListener{
             it.findNavController().navigate(R.id.action_sensorFragment_to_homeFragment)
         }
@@ -70,12 +63,8 @@ class SensorFragment : Fragment() {
         binding.storetap.setOnClickListener{
             it.findNavController().navigate(R.id.action_sensorFragment_to_storeFragment)
         }
-
         return binding.root
     }
 
-    fun changeTempText(string: String){
-        binding.temptext.text = string
-    }
 
 }
