db36612783ca0b5b38e53cc1ee1310dc44c6a4eb
kimdong1144 <82792797+kimdong1144@users.noreply.github.com>
2022-11-16 13:39:29 +0900

    게시판/2

--- app/src/main/java/com/example/plantpet/fragments/StoreFragment.kt
+++ app/src/main/java/com/example/plantpet/fragments/StoreFragment.kt
@@ -1,16 +1,21 @@
 package com.example.plantpet.fragments
 
+import android.app.DownloadManager
+import android.content.Context.DOWNLOAD_SERVICE
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
+import android.webkit.*
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.plantpet.R
 import com.example.plantpet.databinding.FragmentBookmarkBinding
 import com.example.plantpet.databinding.FragmentStoreBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -39,7 +44,32 @@ class StoreFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_store, container, false)
 
         val webView : WebView = view.findViewById(R.id.storeWebView)
-        webView.loadUrl("https://www.google.com/")
+
+        webView.apply{
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            settings.javaScriptEnabled = true
+            settings.javaScriptCanOpenWindowsAutomatically = false
+            settings.setSupportMultipleWindows(false)
+            settings.loadsImagesAutomatically = true
+            settings.useWideViewPort = true
+            settings.loadWithOverviewMode = true
+            settings.setSupportZoom(true)
+            settings.builtInZoomControls = false
+            settings.displayZoomControls = false
+
+            settings.cacheMode = WebSettings.LOAD_DEFAULT
+            settings.setAppCacheEnabled(false)
+            settings.domStorageEnabled = true
+            settings.allowContentAccess
+            settings.userAgentString = "app"
+            settings.defaultTextEncodingName = "UTF-8"
+            settings.databaseEnabled = true
+        }
+
+        webView.loadUrl("https://m.mojongsijang.com")
+
+
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_store, container, false)
         binding.hometap.setOnClickListener {
